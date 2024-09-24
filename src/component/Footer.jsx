ae13c5c7b04215dea9166b1b146f40257e2d7703
M.BasilKhan <basilkhan1250@gmail.com>
2024-09-24 22:20:09 +0500

    sign-in page completed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer">
-            
+            <div className="contact">
+                <p>Question? Contact us.</p>
+            </div>
+            <div className="headings">
+                <p>FAQ</p>
+                <p>Help Center</p>
+                <p>Terms of Use</p>
+                <p>Privacy</p>
+                <p>Cookie Preferences</p>
+                <p>Corporate Information</p>
+            </div>
+            <select className="drop-down" id="cars" name="cars">
+                <option value="saab">English</option>
+            </select>
         </div>
     )
 }
