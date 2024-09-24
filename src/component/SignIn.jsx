f6b36235378704229f7b9eab78ee6b9c98a54686
M.BasilKhan <basilkhan1250@gmail.com>
2024-09-24 20:02:21 +0500

    working on sign-in-page

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import netflix from "./asssets/netflix.png"
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 
 const SignIn = () => {
+    const [showInput, setShowInput] = useState(true)
+    const toggleInput = () => {
+        setShowInput(prevState => !prevState)
+    }
 
     return (
         <div>
@@ -22,16 +27,30 @@ const SignIn = () => {
                             <label class="signin-floating-label" for="username">Email or Phone Number</label>
                         </div>
                         <div class="form-element">
-                            <input type="password" className="signin-input" name="password" id="password" required />
-                            <label class="signin-floating-label" for="password">Password</label>
+                            {showInput && <input type="password" className="signin-input" name="password" id="password" required />}
+                            {showInput && <label class="signin-floating-label" for="password">Password</label>}
                         </div>
                         <button class="sign-btn">Sign In</button>
+                        <p className="or">OR</p>
+                        <button className="sign-in-code" onClick={toggleInput}>
+                            {showInput ? "Use a Sign-In Code" : "Use Password"}
+                        </button>
+                        <p className="password-forgot">Forgot password?</p>
+                        <div className="check-box">
+                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Remember-me" className="custom-checkbox" />
+                            <label for="vehicle1">Remember me</label>
+                        </div>
+                        <div className="back-to-the-page">
+                            <p className="new-netflix">New to Netflix?</p>
+                            <Link className="back-to-page" to='/'>
+                                <p className="back-page">Sign up now.</p>
+                            </Link>
+                        </div>
                     </form>
                 </div>
             </div>
             <Footer></Footer>
         </div>
-
     )
 }
 
