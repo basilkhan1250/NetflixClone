cc396905fde113b2e0b629535b52ca864df52b9b
M.BasilKhan <basilkhan1250@gmail.com>
2024-10-12 22:44:02 +0500

    changes

--- src/component/Trendings.jsx
+++ src/component/Trendings.jsx
@@ -9,7 +9,8 @@ import SexLife from "./asssets/7.png";
 import Crew from "./asssets/8.png";
 import Kalki from "./asssets/9.png";
 import Elite from "./asssets/10.png";
-import MainFooter from "./MainFooter.jsx";
+import MainFooter from "./MainFooter";
+
 
 const Trendings = () => {
     const Images = [
